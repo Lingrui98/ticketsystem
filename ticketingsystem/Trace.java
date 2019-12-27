1f540782fdc17be0dc1578bb49b56ebdb134c5f8
Lingrui98 <goulingrui15@gmail.com>
2019-12-27 17:13:11 +0800

    Do some cleanups

--- ticketingsystem/Trace.java
+++ ticketingsystem/Trace.java
@@ -23,13 +23,13 @@ class ThreadId {
 }
 
 public class Trace {
-	final static int threadnum = 1; // concurrent thread number
+	final static int threadnum = 4; // concurrent thread number
 	final static int routenum = 8; // route is designed from 1 to 3
 	final static int coachnum = 16; // coach is arranged from 1 to 5
 	final static int seatnum = 100; // seat is allocated from 1 to 20
 	final static int stationnum = 10; // station is designed from 1 to 5
 
-	final static int testnum = 50000;
+	final static int testnum = 500000;
 	final static int retpc = 5; // return ticket operation is 10% percent
 	final static int buypc = 20; // buy ticket operation is 30% percent
 	final static int inqpc = 100; //inquiry ticket operation is 60% percent
